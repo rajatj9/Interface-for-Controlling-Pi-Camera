ab25deee16dc46283a447dd48c464c2dfa68688f
Rajat Jain <jain.rajat50@gmail.com>
2019-02-15 15:46:03 +0800

    Added Basic Fields to the form.

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify, json
+import json
 app = Flask(__name__)
 
 @app.route('/')
 def student():
-   return render_template('student.html')
+   return render_template('parameters.html')
 
 @app.route('/result',methods = ['POST', 'GET'])
 def result():
    if request.method == 'POST':
       result = request.form
-      return render_template("result.html",result = result)
+      return jsonify(result)
+      # Instead make a post request to a different end point of the app.
+      # Running the python scripts on the different endpoint.
 
 if __name__ == '__main__':
    app.run(debug = True)
